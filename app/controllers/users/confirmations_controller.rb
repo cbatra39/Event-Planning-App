d6a6186d51eb456e499844c4f36f87dd700914d7
Satyendra6396 <satyendra.kumar@chicmic.co.in>
2023-06-12 11:41:23 +0530

    modified:   app/controllers/users/confirmations_controller.rb 	modified:   config/environments/development.rb

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,15 +3,29 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   skip_before_action :verify_authenticity_token
   respond_to :json
 
-  def create
-    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-    yield resource if block_given?
+  # def create
+  #   self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+  #   yield resource if block_given?
 
+  #   if resource.errors.empty?
+     
+  #     render json: { message: 'Account confirmed successfully.', user: resource }
+  #   else
+  #     render json: { error: 'Unable to confirm account.' }, status: :unprocessable_entity
+  #   end
+  # end
+
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+  
+    yield resource if block_given?
+  
     if resource.errors.empty?
       resource.update(status: User.statuses[:active], is_verified: User.statuses[:verified],unconfirmed_email: "confirmed")
-      render json: { message: 'Account confirmed successfully.', user: resource }
+      render json: { message: 'Confirmation successful.' }
     else
-      render json: { error: 'Unable to confirm account.' }, status: :unprocessable_entity
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
   end
+  
 end
